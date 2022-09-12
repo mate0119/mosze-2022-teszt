a1efeec498bceec2070eaca812a26897a9116985
mate0119 <mmate0119@gmail.com>
2022-09-12 15:12:47 +0200

    hibak kommentelve

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hibás konstans változó név
+    std::cout << '1-100 ertekek duplazasa'; //sortörés hiányzik
+    for (int i = 0) //for ciklus hiányos
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //statement hiányzik (végtelen ciklus)
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:" //nincs kiírva az érték
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //nincs inicializálva az atlag
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //vesszõ hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
